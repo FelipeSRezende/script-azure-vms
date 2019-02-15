2cec8bff80b22c656d31b0b73309460806d1460e
Felipe Rezende <frezende21@gmail.com>
2019-02-15 01:42:14 -0200

    melhorias de login no azure

--- create VM.ps1
+++ create VM.ps1
@@ -1,25 +1,22 @@
-﻿#Logar na minha subscrição
-
-az login
+﻿#Logar na subscrição
+az login -u -p # inserir usuário e senha para pular browser
 
 
 #Listar resourve groups disponíveis
-
 az group list
 
 #cRiar um resource group
 az group create --name myResourceGroupVM --location eastus
 
 #Listar imagens de S.O.s mais populares
-
 az vm image list --output table
 
 
 #Listar imagens de S.O.s específicos
-
 az vm image list --offer <#Nome do S.O.#> --all --output table
 
 #Criar VM via CLI
-
 az vm create \ --resource-group rg-lab-testes\--name vm-lab-teste01 \ --image
+
+
 
